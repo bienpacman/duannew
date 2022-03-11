fb421ee108f3aed9b9c40d4fbf76f1ed82e0132a
bienpacman <biendz2000@gmail.com>
2022-03-12 00:37:00 +0700

    sua doi

--- Student.js
+++ Student.js
@@ -1,8 +1,9 @@
 class Student {
-    constructor(id, name, age) {
+    constructor(id, name, age,position) {
         this.id = id
         this.name = name
         this.age = age
+        this.position = position
     }
 
     getId() {
@@ -14,18 +15,24 @@ class Student {
     }
 
     getName() {
-        return this.name
+        return this.name;
     }
 
     setNam(name) {
-        this.name = name
+        this.name = name;
     }
 
     getAge() {
-        return this.age
+        return this.age;
     }
 
     setAge(age) {
-        this.age = age
+        this.age = age;
+    }
+    getPosition() {
+        return this.position;
+    }
+    setPosition(position){
+        this.position = position;
     }
 }
